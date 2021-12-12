6c2d16fb6472669d33b98a6df70b2dd56ae3fa15
Mohammad Mohammad <mmohammadq8@outlook.com>
2021-12-12 09:08:41 +0300

    redirect to www

--- mshroo3i-api/Program.cs
+++ mshroo3i-api/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Rewrite;
 using mshroo3i_api;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +7,11 @@ builder.Services.AddControllers();
 builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
+
+// TODO: Do this at the DNS level using CloudFare
+// See https://app.asana.com/0/1201502066479091/1201502862005579/f
+var options = new RewriteOptions().AddRedirectToWwwPermanent();
+app.UseRewriter(options);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
