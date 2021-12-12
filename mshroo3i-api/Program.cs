29819b4ed74d3a5a40a5e181ec7f4413654fb7b4
Mohammad Mohammad <mmohammadq8@outlook.com>
2021-12-12 09:11:53 +0300

    undo redirect

--- mshroo3i-api/Program.cs
+++ mshroo3i-api/Program.cs
@@ -8,11 +8,6 @@ builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
 
-// TODO: Do this at the DNS level using CloudFare
-// See https://app.asana.com/0/1201502066479091/1201502862005579/f
-var options = new RewriteOptions().AddRedirectToWwwPermanent();
-app.UseRewriter(options);
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
