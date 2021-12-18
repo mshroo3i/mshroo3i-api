93cd2d4c5c48dde5864948ba961afbcd117706be
Mohammad Mohammad <mmohammadq8@outlook.com>
2021-12-19 01:51:56 +0300

    rename product image response

--- mshroo3i-api/Responses/ProductResponse.cs
+++ mshroo3i-api/Responses/ProductResponse.cs
@@ -5,7 +5,7 @@ public class ProductResponse
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string Image { get; set; } = string.Empty;
+    public string ImageSrc { get; set; } = string.Empty;
     public double Price { get; set; } = 0;
     public IList<ProductOptionResponse> ProductOptions { get; set; } = new List<ProductOptionResponse>();
 }
